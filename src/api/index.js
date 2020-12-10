38c22a4805940e2ae488011b85d2a6ec98418eba
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-12-09 19:02:31 -0700

    users are set in state on mount

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,8 @@ import React from 'react'
 var users = require("./users.json");
 
 function API() {
-
+    
     return (users)
 }
 
-export default users
+export default API
--- src/components/TableRows.js
+++ src/components/TableRows.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import users from './../api/index'
+import API from './../api/index';
 
-function TableRows () {
+class TableRows extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            users: []
+        }
+    }
 
+    componentDidMount() {
+        var usersInit = API()
+        this.setState({
+            isLoaded: true,
+            users: usersInit
+        })
+    }
 
-    return (
-        <tr>hello</tr>
-    )
-
+    render() {
+        return (
+            <tr>hello</tr>
+        )
+    }
 
 }
+
+
 
 export default TableRows;
